f8e2b9bdac26afb0026f0c0f9020da47b935959a
Vladimir <shaitan.vladimir@gmail.com>
2021-07-01 18:56:27 +0300

    init

---
+++ js/array_methods/arr_delete.js
@@ -0,0 +1,11 @@
+const array = ['apple', 'banana', 'grapefruit', 'orange', 'potato', 'cucumber', 'melon', 'cherry'];
+
+
+// delete - удаляем только значение элемента массива по индексу
+// но после удаления остается пустое место а длинна массива не уменьшется
+
+delete array[3];
+console.log(array);
+console.log(array.length);
+
+//
---
+++ js/array_methods/arr_includes.js
@@ -0,0 +1,28 @@
+let arr = ['Alex', 'Vlad', 'Helen', 'Yuriy'];
+
+
+// arr.indexOf(item, from) ищет item, начиная с индекса from,
+// и возвращает индекс, на котором был найден искомый элемент, в противном случае -1.
+
+// console.log( arr.indexOf('banana') );
+//
+// console.log( arr.indexOf('Alex')  );
+//
+// console.log( arr.indexOf('Helen') );
+
+// arr.lastIndexOf(item, from) – то же самое, но ищет справа налево.
+
+// console.log(arr.lastIndexOf('banana'));
+//
+// console.log(arr.lastIndexOf('Alex'));
+//
+// console.log(arr.lastIndexOf('Helen'));
+
+// arr.includes(item, from) – ищет item, начиная с индекса from,
+// и возвращает true, если поиск успешен.
+
+// console.log(arr.includes('banana'));
+//
+// console.log(arr.includes('Helen'));
+//
+// console.log(arr.includes('Helen'), 1);
